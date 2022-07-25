32ed5886e0b3993ce09a90751556f3962d91a46f
alexsis-valentinovich <alexsis-valentinovich@gmail.com>
2022-07-25 18:28:09 +0500

    Добавьте файлы проекта.

---
+++ Rart_1/ISeries.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rart_1
+{
+    interface ISeries
+    {
+        void SetStart(int x);
+        int GetNext();
+        void Reset();
+    }
+    class ArithProgression : ISeries
+    {
+        int step;
+        int startPoint;
+        int result;
+        public int GetNext()
+        {
+            result+=step;
+            return result;
+        }
+
+        public void Reset()
+        {
+            result = startPoint;
+        }
+
+        public void SetStart(int x)
+        {
+            startPoint =x;
+            result = startPoint;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+
+    class GeomProgression : ISeries
+    {
+        int step;
+        int startPoint;
+        int result;
+        public int GetNext()
+        {
+            result*=step;
+            return result;
+        }
+
+        public void Reset()
+        {
+            result=startPoint;
+        }
+
+        public void SetStart(int x)
+        {
+            startPoint=x;
+            result = startPoint;
+        }
+        public void setStep(int s)
+        {
+            step = s;
+        }
+    }
+}
---
+++ Rart_1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Разработать интерфейс ISeries генерации ряда чисел. Интерфейс содержит следующие элементы:
+
+//метод void SetStart(int x) -устанавливает начальное значение
+//метод int GetNext() -возвращает следующее число ряда
+//метод void Reset() -выполняет сброс к начальному значению
+//Разработать классы ArithProgression и GeomProgression, которые реализуют интерфейс ISeries.
+//В классах реализовать методы интерфейса в соответствии с логикой арифметической и геометрической прогрессии
+//соответственно.
+namespace Rart_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ариметическая: начальная установка");
+            Console.Write("Ввeдите начальное значение = ");
+            int startPointA = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ввeдите шаг = ");
+            int stepA = Convert.ToInt32(Console.ReadLine());
+            ArithProgression arithPro = new ArithProgression();
+            arithPro.SetStart(startPointA);
+            arithPro.setStep(stepA);
+            Console.WriteLine(startPointA);
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            arithPro.Reset();
+            Console.WriteLine("Ариметическая: сброс");
+            Console.WriteLine(startPointA);
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine(arithPro.GetNext());
+            Console.WriteLine();
+            GeomProgression geomPro = new GeomProgression();
+            Console.WriteLine("Геометрическая: начальная установка");
+            Console.Write("Ввeдите начальное значение = ");
+            int startPointG = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ввeдите шаг = ");
+            int stepG = Convert.ToInt32(Console.ReadLine());
+            geomPro.SetStart(startPointG);
+            geomPro.setStep(stepG);
+            Console.WriteLine(startPointG);
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            
+            geomPro.Reset();
+            Console.WriteLine("Геометрическая: сброс");
+            Console.WriteLine(startPointG);
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            Console.WriteLine(geomPro.GetNext());
+            Console.ReadKey();
+        }
+    }
+}
